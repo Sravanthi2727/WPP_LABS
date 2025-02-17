b2e21890beacc2360a3690dccaad9fc18a5fa962
SRAVANTHI VADLA <137860943+Sravanthi2727@users.noreply.github.com>
2025-02-17 09:45:15 +0530

    q1 q2 uploaded
    q2 should be updated

---
+++ LAB1/l1q2.py
@@ -0,0 +1,51 @@
+a = 10
+b = 5
+
+print("Performing Arithematic Operations :- \n")
+print("a and b values are :", a, b)
+print("+:", a+b)
+print("-:", a-b)
+print("*:", a*b)
+print("/:", a/b)
+print("//:", a//b)
+print("%:", a%b)
+print("**:", a**b)
+
+print("Performing Assignment Operations :- \n")
+print("a and b values are :", a, b)
+a+=b
+print("+=:", a)
+a-=b
+print("-=:", a)
+a*=b
+print("*=:", a)
+a/=b
+print("/=:", a)
+a//=b
+print("//=:", a)
+
+
+print("Performing Relational Operations :- \n")
+a = 5
+b = 10
+print("a and b values are :", a, b)
+print(">=:", a>=b)
+print("<=:", a<=b)
+print("==:", a==b)
+print("!=:", a!=b)
+
+
+print("Performing Bitwise Operations :- \n")
+a = 5
+b = 10
+print("a and b values are :", a, b)
+print("|:", a|b)
+print("&:", a&b)
+print("^:", a^b)
+
+
+print("Performing Binary Operations :- \n")
+print("a and b values are :", a, b)
+print("and:", a and b)
+print("or:", a or b)
+
